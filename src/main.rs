0f39a975a90b943aef2a16f4ea3c7a1d600fcab0
Simeon Widdis <sawiddis@gmail.com>
2022-08-21 11:28:55 -0600

    Add acceleration

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,26 @@ impl Vector3 {
     fn to_string(&self) -> String {
         format!("<{}, {}, {}>", self.x, self.y, self.z)
     }
+
+    fn add(&self, v: &Vector3) -> Vector3 {
+        Vector3 {
+            x: self.x + v.x,
+            y: self.y + v.y,
+            z: self.z + v.z
+        }
+    }
+
+    fn sub(&self, v: &Vector3) -> Vector3 {
+        Vector3 {
+            x: self.x - v.x,
+            y: self.y - v.y,
+            z: self.z - v.z
+        }
+    }
+
+    fn norm(&self) -> f64 {
+        return (self.x * self.x + self.y * self.y + self.z * self.z).sqrt();
+    }
 }
 
 struct Body {
@@ -40,6 +60,39 @@ impl Body {
             velocity: self.velocity.clone(),
         }
     }
+
+    fn force_vector(&self, body: &Body) -> Vector3 {
+        const G: f64 = 1.0;
+        let d = self.position.sub(&body.position);
+        let r = d.norm();
+        if r == 0.0 {
+            Vector3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0
+            }
+        } else {
+            let d_sum = d.x.abs() + d.y.abs() + d.z.abs();
+            let f_g = G * self.mass * body.mass / (r * r);
+            Vector3 {
+                x: f_g * d.x / d_sum,
+                y: f_g * d.y / d_sum,
+                z: f_g * d.z / d_sum
+            }
+        }
+    }
+
+    fn accelerate(&self, force: &Vector3, step: f64) -> Body {
+        return Body {
+            mass: self.mass,
+            position: self.position.clone(),
+            velocity: Vector3 {
+                x: self.velocity.x + force.x / self.mass * step,
+                y: self.velocity.y + force.y / self.mass * step,
+                z: self.velocity.z + force.z / self.mass * step
+            }
+        }
+    }
 }
 
 struct Universe {
@@ -55,10 +108,29 @@ impl Universe {
 
     fn tick(&self) -> Universe {
         const STEP: f64 = 0.5;
+        let forces = self.force_vectors();
         Universe {
             time: self.time + STEP,
-            bodies: self.bodies.iter().map(|b| b.tick(STEP)).collect(),
+            bodies: self.bodies.iter()
+                .enumerate()
+                .map(|(i, b)| b.accelerate(&forces[i], STEP).tick(STEP))
+                .collect()
         }
+    }
+    
+    fn force_vectors(&self) -> Vec<Vector3> {
+        let mut forces: Vec<Vector3> = self.bodies.iter()
+            .map(|_| Vector3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0
+            }).collect();
+        for (i, a) in self.bodies.iter().enumerate() {
+            for b in self.bodies.iter() {
+                forces[i] = forces[i].add(&a.force_vector(b));
+            }
+        }
+        return forces;
     }
 }
 
