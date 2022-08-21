344036d2ae85abe3162d58981bd98078f185dc58
Simeon Widdis <sawiddis@gmail.com>
2022-08-21 00:27:43 -0600

    Invent the universe

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,75 @@
+struct Vector3 {
+    x: f32,
+    y: f32,
+    z: f32
+}
+
+impl Vector3 {
+    fn to_string(&self) -> String {
+        format!("<{}, {}, {}>", self.x, self.y, self.z)
+    }
+}
+
+struct Body {
+    mass: f32,
+    position: Vector3,
+    velocity: Vector3
+}
+
+impl Body {
+    fn to_string(&self) -> String {
+        let position = self.position.to_string();
+        let momentum = Vector3 {
+            x: self.velocity.x * self.mass,
+            y: self.velocity.y * self.mass,
+            z: self.velocity.z * self.mass
+        }.to_string();
+        format!("[{} {}]", position, momentum)
+    }
+}
+
+struct Universe {
+    time: f32,
+    bodies: Vec<Body>
+}
+
+impl Universe {
+    fn to_string(&self) -> String {
+        let bodies: Vec<String> = self.bodies
+                .iter()
+                .map(|x| x.to_string())
+                .collect();
+        format!("{}: {{{}}}", self.time, bodies.join(" "))
+    }
+
+    fn tick(&mut self) {
+        self.time += 1.0;
+    }
+}
+
+fn create_universe() -> Universe {
+    Universe {
+        time: 0.0,
+        bodies: vec![
+            Body {
+                mass: 1.0,
+                position: Vector3 { x: 1.0, y: 0.0, z: 0.0 },
+                velocity: Vector3 { x: 0.0, y: 1.0, z: 0.0 }
+            },
+            Body {
+                mass: 1.0,
+                position: Vector3 { x: -1.0, y: 0.0, z: 0.0 },
+                velocity: Vector3 { x: 0.0, y: -1.0, z: 0.0 }
+            }
+        ]
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut universe = create_universe();
+    println!("{}", universe.to_string());
+    for _ in 0..20 {
+        universe.tick();
+        println!("{}", universe.to_string());
+    }
 }
