fded985231de7d5d2b8034fb9e111e1c8755b516
Simeon Widdis <sawiddis@gmail.com>
2022-08-22 12:51:09 -0600

    Add forces to bodies

--- src/physics.rs
+++ src/physics.rs
@@ -9,9 +9,15 @@ fn add_bodies(rigid_body_set: &mut RigidBodySet, collider_set: &mut ColliderSet,
         let rigid_body = RigidBodyBuilder::dynamic()
             .translation(vector![body.position.x, body.position.y, body.position.z])
             .build();
-        let collider = ColliderBuilder::ball(radius).restitution(1.0).build();
+        let collider = ColliderBuilder::ball(radius).restitution(1.0).density(1.0).build();
         let handle = rigid_body_set.insert(rigid_body);
         collider_set.insert_with_parent(collider, handle, rigid_body_set);
+    }
+}
+
+fn add_forces(rigid_body_set: &mut RigidBodySet) {
+    for body in rigid_body_set.iter_mut() {
+        body.1.add_force(vector![10.0, 20.0, 30.0], true);
     }
 }
 
@@ -20,4 +26,5 @@ pub fn load_physics(config: ThreeBodyConfig) {
     let mut collider_set = ColliderSet::new();
 
     add_bodies(&mut rigid_body_set, &mut collider_set, &config.universe.bodies);
+    add_forces(&mut rigid_body_set);
 }
