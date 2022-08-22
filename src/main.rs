688fc7319d109f1c2a3628950e5d853cc2a0eb62
Simeon Widdis <sawiddis@gmail.com>
2022-08-22 11:38:12 -0600

    Switch to dedicated config struct

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,43 @@
-use std::collections::HashMap;
-
-use config::{Config, File, FileFormat, Value};
 use kiss3d::light::Light;
 use kiss3d::nalgebra::geometry::Translation3;
 use kiss3d::scene::SceneNode;
 use kiss3d::window::Window;
-use serde::Deserialize;
 
-use crate::physics::Universe;
+use crate::physics::{Universe, Body, Vector3};
+use crate::sim_config::{Universe as SimUniverse, load_config};
 
 pub mod physics;
+pub mod sim_config;
 
-#[derive(Deserialize)]
-struct Constants {
-    time_step: f64,
-}
-
-fn load_config(filename: &str) -> Config {
-    Config::builder()
-        .add_source(File::new(filename, FileFormat::Yaml))
-        .build()
-        .expect("Configuration is valid")
-}
-
-fn load_constants(config: HashMap<String, Value>) -> Constants {
-    Constants {
-        time_step: config["time_step"].clone().try_deserialize().unwrap()
-    }
-}
-
-fn create_universe(config: HashMap<String, Value>) -> Universe {
-    Universe {
+fn create_universe(config: SimUniverse) -> Universe {
+    physics::Universe {
         time: 0.0,
-        grav_const: config["grav_const"].clone().try_deserialize().unwrap(),
-        bodies: config["bodies"].clone().try_deserialize().unwrap(),
+        grav_const: config.grav_const,
+        bodies: config.bodies.iter().map(|b| Body {
+            mass: b.mass,
+            velocity: Vector3 {
+                x: b.velocity.x,
+                y: b.velocity.y,
+                z: b.velocity.z
+            },
+            position: Vector3 {
+                x: b.position.x,
+                y: b.position.y,
+                z: b.position.z
+            }
+        }).collect()
     }
 }
 
 fn main() {
     let config = load_config("config.yaml");
-    let constants = load_constants(config.get_table("constants").unwrap());
-    let mut universe = create_universe(config.get_table("universe").unwrap());
+    let mut universe = create_universe(config.universe);
 
     let mut window = Window::new("Kiss3d: cube");
     let mut spheres: Vec<SceneNode> = Vec::new();
 
     for (i, body) in universe.bodies.iter().enumerate() {
-        spheres.push(window.add_sphere(0.1));
+        spheres.push(window.add_sphere(1.0));
         spheres[i].set_local_translation(Translation3::new(
             body.position.x as f32,
             body.position.y as f32,
@@ -56,7 +47,7 @@ fn main() {
 
     window.set_light(Light::StickToCamera);
     while window.render() {
-        universe = universe.tick(constants.time_step);
+        universe = universe.tick(config.constants.time_step);
         for (i, body) in universe.bodies.iter().enumerate() {
             spheres[i].set_local_translation(Translation3::new(
                 body.position.x as f32,
---
+++ src/sim_config.rs
@@ -0,0 +1,41 @@
+use config::{Config, File, FileFormat};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Constants {
+    pub time_step: f64
+}
+
+#[derive(Deserialize)]
+pub struct Vector3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64
+}
+
+#[derive(Deserialize)]
+pub struct Body {
+    pub mass: f64,
+    pub position: Vector3,
+    pub velocity: Vector3
+}
+
+#[derive(Deserialize)]
+pub struct Universe {
+    pub grav_const: f64,
+    pub bodies: Vec<Body>
+}
+
+#[derive(Deserialize)]
+pub struct ThreeBodyConfig {
+    pub constants: Constants,
+    pub universe: Universe
+}
+
+pub fn load_config(filename: &str) -> ThreeBodyConfig {
+    let config = Config::builder()
+    .add_source(File::new(filename, FileFormat::Yaml))
+    .build()
+    .expect("Yaml is valid");
+    config.try_deserialize::<ThreeBodyConfig>().expect("Configuration is valid")
+}
